053298627494d3d760204431f2ef75fb1bd20676
Rida Kodad <kodadreda@gmail.com>
2019-08-03 11:22:15 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,8 +2,9 @@ require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
     current_year =  Time.now.year
-    Birth year = 1984
-
-    expect(age_of_person).to eq(19)
+    birth_year = 1984
+    age = current_year - birth_year
+    age_of_person = current_age_for_birth_year
+    expect(age_of_person).to eq(age)
   end
 end
